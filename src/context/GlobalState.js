f8245d223284590bf0cc8cc9ae372396bfb61896
Sanmi01 <supersanmi01@gmail.com>
2022-11-17 06:44:50 +0100

    added functionalities

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -18,9 +18,22 @@ export const GlobalContext = createContext(initialState);
 // Provider
 export const GlobalProvider = ({ children }) => {
     const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    // Actions
+    function deleteTransaction(id) {
+      dispatch({ type: "DELETE_TRANSACTION", payload: id });
+    }
+
+    function addTransaction(transaction) {
+      dispatch({ type: "ADD_TRANSACTION", payload: transaction });
+    }
     
         return (
-            <GlobalContext.Provider value={{ transactions: state.transactions }}>
+            <GlobalContext.Provider value={{ 
+              transactions: state.transactions,
+              deleteTransaction,
+              addTransaction
+              }}>
                 {children}
             </GlobalContext.Provider>
         );
